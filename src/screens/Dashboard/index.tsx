daf5a0ff58a4f05faeb78cb3ff38a6dedeadd2f1
Lopes, Matheus P <matheus.p.lopes@accenture.com>
2022-04-28 19:42:40 -0300

    added rn responsive font size, and image

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    Photo,
+    User,
+    UserGreeting, UserInfo,
+    UserName, UserWrapper
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/16311630?v=4' }} 
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Matheus</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+                
+            </Header>
         </Container>
     )
 }
